ffb2af2ecc0166f842324e80d311b32dc5104cab
Ajdin <ajdinproho@gmail.com>
2019-01-06 18:02:57 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,14 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <textarea v-model="noviTekst" id="textBox">
+
+    </textarea>
+    <button v-on:click="dodaj" id="dugme">Dodaj</button>
+
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="biljeska in nizTekst" :key="biljeska.id">{{ biljeska }}
+        <button v-on:click="izbrisi(biljeska)" id="dugmeIzbrisi"> X </button>
+      </li>
     </ul>
   </div>
 </template>
@@ -33,8 +16,19 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return {
+      noviTekst: '',
+      nizTekst: [],
+    };
+  },
+  methods: {
+    dodaj() {
+       this.nizTekst.push(this.noviTekst);
+   },
+    izbrisi(biljeska) {
+      this.nizTekst.splice(this.nizTekst.indexOf(biljeska),1);
+    },
   },
 };
 </script>
@@ -51,8 +45,44 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
+  width:30%;
+  border: 3px solid black;
+  margin-top: 7px;
+  height:auto;
+  background:darkblue;
+  border-radius: 9px;
+  color:white;
 }
 a {
   color: #42b983;
+}
+#dugme {
+  border:2px solid darkblue;
+  border-radius: 22px;
+  height:50px;
+  width:100px;
+  margin-left:5px;
+  position:absolute;
+  top:125px;
+  color:white;
+  background: darkblue;
+  outline: none;
+}
+#textBox {
+  resize: none;
+  height:160px;
+  width:500px;
+  border: 3px solid darkblue;
+  border-radius: 33px;
+  outline: none;
+  padding:20px;
+  font-size: 20px;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  padding:10px;
+}
+#dugmeIzbrisi {
+  float:right;
+  color:crimson;
+  border-radius: 9px;
 }
 </style>
